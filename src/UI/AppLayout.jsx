83df9a431d9f05b16264894aafe06494e4b03dd6
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-09 19:40:24 +0100

    App layout router creation

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -1,26 +1,28 @@
-import { Outlet, useNavigation } from "react-router-dom"
-import CartOverview from "../Features/Cart/CartOverview"
-import Header from "./Header"
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../Features/Cart/CartOverview";
+import Header from "./Header";
 import Loader from "./Loader";
 
 function AppLayout() {
- const navigation = useNavigation();
+  const navigation = useNavigation();
   // console.log(navigation);
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
-      {isLoading && <Loader/>}
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-      <Header/>
+      <Header />
 
-      <main>
-        <Outlet/>
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-      <CartOverview/>
+      <CartOverview />
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
