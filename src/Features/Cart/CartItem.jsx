169d21dbddb22bcaadc8c7345421d1fe23eca2c7
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-11 15:12:55 +0100

    Cart Item Component

--- src/Features/Cart/CartItem.jsx
+++ src/Features/Cart/CartItem.jsx
@@ -6,11 +6,11 @@ function CartItem({ item }) {
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
-      <p className="mb-1">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between">
-        <p className="text-sm">{formatCurrency(totalPrice)}</p>
+        <p className="text-sm pr-4">{formatCurrency(totalPrice)}</p>
         <Button type="small">Delete</Button>
       </div>
     </li>
