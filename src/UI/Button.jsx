bb12ea534a9d19dffe50cf69665a98b395e03c02
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-11 15:14:31 +0100

    Button style component

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
-
-const base= `mt-2 inline-block rounded-full bg-yellow-400
+  const base = `mt-2 text-sm inline-block rounded-full bg-yellow-400
     font-semibold uppercase tracking-wide text-stone-800 
     transition-colors duration-300 hover:bg-yellow-300 
     focus:bg-yellow-300 focus:outline-none focus:ring 
@@ -10,9 +9,15 @@ const base= `mt-2 inline-block rounded-full bg-yellow-400
     disabled:cursor-not-allowed`;
 
   const styles = {
-   primary: base + ` px-4 py-3 md:px-6 md:py-4`,
-   small: base + ` px-3 py-1.5 md:px-5 md:py-2.5 text-xs`
-  }  
+    primary: base + ` px-4 py-3 md:px-6 md:py-4`,
+    small: base + ` px-3 py-1.5 md:px-5 md:py-2.5 text-xs`,
+    secondary: `mt-2 text-sm inline-block rounded-full border-2 border-stone-300
+   font-semibold uppercase tracking-wide  px-4 py-2.5 md:px-6 md:py-3.5 text-stone-400 
+   transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800
+   focus:bg-stone-300 focus:outline-none focus:ring 
+   focus:ring-stone-200 focus:ring-offset-2 focus-text-stone-800
+   disabled:cursor-not-allowed`,
+  };
 
   if (to)
     return (
