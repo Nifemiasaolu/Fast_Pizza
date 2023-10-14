a162e0ced9452136903ebcc49bc672dcdc39d935
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-14 23:00:51 +0100

    Update: functionalized increment function

--- src/Features/Cart/CartItem.jsx
+++ src/Features/Cart/CartItem.jsx
@@ -1,17 +1,28 @@
+import { useSelector } from "react-redux";
 import { formatCurrency } from "../../Utilities/helpers";
 import DeleteItem from "./DeleteItem";
+import UpdateItemQuantity from "./UpdateItemQuantity";
+import { getCurrentQuantityById } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const currentQuantity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
-    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+    <li className="py-3 sm:flex sm:items-center sm:justify-between ">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="pr-4 text-sm">{formatCurrency(totalPrice)}</p>
-        <DeleteItem pizzaId={pizzaId} />
+
+        <div className="flex gap-3">
+          <UpdateItemQuantity
+            pizzaId={pizzaId}
+            currentQuantity={currentQuantity}
+          />
+          <DeleteItem pizzaId={pizzaId} />
+        </div>
       </div>
     </li>
   );
